6ad93669a7842d2b3166a16886d708519f7deba2
Ramon Dantas de Jesus <ramon100.black@gmail.com>
2011-09-12 19:29:19 -0300

    Local variables and Garbage Collection

--- graphicaljokenpo.lua
+++ graphicaljokenpo.lua
@@ -1,10 +1,12 @@
 require "lang"
 require "libjokenpo"
 require "iuplua"
+local langs, nlangs, res, idlang, choi
 langs = {}
 nlangs = {}
 for k,v in pairs(trans) do table.insert(langs, tostring(k)); table.insert(nlangs, trans[tostring(k)].desc) end
 function main()
+  local res, cho, label, dlg, timer, jogo, r
   res, cho = iup.GetParam(trans[clang].escolha_jogada, nil, trans[clang].esc_sorte.." %l|"..table.concat(choi, "|").."|\n", 0)
   if res then
     label = iup.label{ title = trans[clang].cpu_escolhendo }
@@ -15,7 +17,6 @@ function main()
       jogo = Jogo:novo(cho+1)
       jogo:jogar()
       dlg:destroy()
-      label = nil
       if jogo:ganhei() then
 	iup.Message("Jokenpo", trans[clang].ganhou)
       elseif jogo:ganhei() == nil then
@@ -25,7 +26,11 @@ function main()
       end
       self.run = "NO"
       r = iup.Alarm("Jokenpo", trans[clang].jogar_novo, trans[clang].sim, trans[clang].nao)
-      if r == 1 then main() end
+      if r == 1 then
+	main()
+      else
+	os.exit(0)
+      end
     end
     timer.run = "YES"
     iup.MainLoop()
@@ -33,7 +38,10 @@ function main()
 end
 res, idlang = iup.GetParam("Language", nil, "Choose your language: %l|"..table.concat(nlangs, "|").."|\n", 0)
 if res then
+  res = nil
   clang = langs[idlang+1]
+  idlang = nil
+  nlangs = nil
   choi = {trans[clang].pedra, trans[clang].papel, trans[clang].tesoura}
   main()
 end
